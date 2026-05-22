5992fb21d08b856b705db116ee3c666614f1db51
Nobberu <itsnobberu@gmail.com>
2026-05-22 13:10:49 +0700

    Forgot commit before Quiz 2

---
+++ CaseMethod02/src/Pesanan21.java
@@ -0,0 +1,10 @@
+public class Pesanan21 {
+    int kodePesanan, harga;
+    String namaPesanan;
+
+    public Pesanan21(int kodePesanan, String namaPesanan, int harga) {
+        this.kodePesanan = kodePesanan;
+        this.namaPesanan = namaPesanan;
+        this.harga = harga;
+    }
+}
